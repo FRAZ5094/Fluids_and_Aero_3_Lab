80188904b8f91720eb84d677ac51218509b6a95f
FRAZ5094 <frasergmorrison@btinternet.com>
2021-01-27 22:14:00 +0000

    delta wing stuff

--- Deltawing.m
+++ Deltawing.m
@@ -5,23 +5,33 @@ clear
 
 data=readtable("data/semester_2(Delta_wing)/DeltaWing2020.csv");
 
+%data when wind tunnel off 
 data_off=data(1:23,:);
 
+%data when wind tunnel at 20m/s
 data_on=data(24:end,:);
 
 %data=sortrows(data,"Incidence");
 
 
 %calculate lift using F_z and angle of attack
-lift=data_on.Fz_L_.*cos(deg2rad(data_on.Incidence));
+lift_on=data_on.Fz_L_.*cos(deg2rad(data_on.Incidence));
+lift_off=data_off.Fz_L_.*cos(deg2rad(data_off.Incidence));
 
 %calcualte drag using F_x and angle of attack
-drag=data_on.Fx_D_.*cos(deg2rad(data_on.Incidence));
+drag_on=data_on.Fx_D_.*cos(deg2rad(data_on.Incidence));
+drag_off=data_off.Fx_D_.*cos(deg2rad(data_off.Incidence));
 
+
+lift=lift_on-lift_off;
+drag=drag_on-drag_off;
 
 figure(1);
 plot(data_on.Incidence,lift);
-legend("lift (N)");
+legend("lift (N)","Location","southeast");
+xlim([0,34]);\
 figure(2);
 plot(data_on.Incidence,drag);
-legend("drag (N)");
+ylim([0,max(drag)*1.2]);
+xlim([0,34]);
+legend("drag (N)","Location","southeast");
